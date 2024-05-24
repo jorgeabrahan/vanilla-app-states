1594506f45f85058ec6edba370c04d8fcce9184b
jorgeabrahan <jorge24abrahan@gmail.com>
2024-05-23 23:02:17 -0600

    update: Transpile code changes

--- State/AppState.js
+++ State/AppState.js
@@ -9,100 +9,107 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _DOMHandler_instances, _DOMHandler_stateElements, _DOMHandler_stateId, _DOMHandler_stateWrapper, _DOMHandler_isCustomRenderEnabled, _DOMHandler_onRender, _DOMHandler_handleCustomRendering, _DOMHandler_handleNativeRendering, _AppState_instances, _AppState_stateId, _AppState_stateInitial, _AppState_stateCurrent, _AppState_isCustomRenderEnabled, _AppState_domHandler, _AppState_changeState;
-const isOneOfType = (types, value) => types.some((type) => typeof value === type);
-class DOMHandler {
+var _DOMUpdatesHandler_instances, _DOMUpdatesHandler_stateElements, _DOMUpdatesHandler_stateId, _DOMUpdatesHandler_stateWrapper, _DOMUpdatesHandler_isCustomRenderEnabled, _DOMUpdatesHandler_onRender, _DOMUpdatesHandler_handleObjectStateRendering, _DOMUpdatesHandler_handleSingleValueStateRendering, _State_instances, _State_stateId, _State_stateInitial, _State_stateCurrent, _State_isCustomRenderEnabled, _State_domHandler, _State_changeState;
+export class DOMUpdatesHandler {
     constructor(stateId, stateWrapper, isCustomRenderEnabled, onRender) {
-        _DOMHandler_instances.add(this);
-        _DOMHandler_stateElements.set(this, void 0);
-        _DOMHandler_stateId.set(this, void 0);
-        _DOMHandler_stateWrapper.set(this, void 0);
-        _DOMHandler_isCustomRenderEnabled.set(this, void 0);
-        _DOMHandler_onRender.set(this, void 0);
-        __classPrivateFieldSet(this, _DOMHandler_stateId, stateId, "f");
-        __classPrivateFieldSet(this, _DOMHandler_stateWrapper, stateWrapper, "f");
-        __classPrivateFieldSet(this, _DOMHandler_stateElements, Array.from(__classPrivateFieldGet(this, _DOMHandler_stateWrapper, "f").querySelectorAll(`[data-state="${__classPrivateFieldGet(this, _DOMHandler_stateId, "f")}"]`)), "f");
-        __classPrivateFieldSet(this, _DOMHandler_isCustomRenderEnabled, isCustomRenderEnabled, "f");
-        __classPrivateFieldSet(this, _DOMHandler_onRender, onRender, "f");
+        _DOMUpdatesHandler_instances.add(this);
+        _DOMUpdatesHandler_stateElements.set(this, void 0);
+        _DOMUpdatesHandler_stateId.set(this, void 0);
+        _DOMUpdatesHandler_stateWrapper.set(this, void 0);
+        _DOMUpdatesHandler_isCustomRenderEnabled.set(this, void 0);
+        _DOMUpdatesHandler_onRender.set(this, void 0);
+        __classPrivateFieldSet(this, _DOMUpdatesHandler_stateId, stateId, "f");
+        __classPrivateFieldSet(this, _DOMUpdatesHandler_stateWrapper, stateWrapper, "f");
+        __classPrivateFieldSet(this, _DOMUpdatesHandler_isCustomRenderEnabled, isCustomRenderEnabled, "f");
+        __classPrivateFieldSet(this, _DOMUpdatesHandler_stateElements, Array.from(__classPrivateFieldGet(this, _DOMUpdatesHandler_stateWrapper, "f").querySelectorAll(`[data-state="${__classPrivateFieldGet(this, _DOMUpdatesHandler_stateId, "f")}"]`)), "f");
+        __classPrivateFieldSet(this, _DOMUpdatesHandler_onRender, onRender, "f");
     }
-    updateElements(state) {
-        for (const element of __classPrivateFieldGet(this, _DOMHandler_stateElements, "f")) {
-            if (__classPrivateFieldGet(this, _DOMHandler_isCustomRenderEnabled, "f")) {
-                __classPrivateFieldGet(this, _DOMHandler_instances, "m", _DOMHandler_handleCustomRendering).call(this, state, element);
+    updateDOMElements(state) {
+        for (const element of __classPrivateFieldGet(this, _DOMUpdatesHandler_stateElements, "f")) {
+            if (__classPrivateFieldGet(this, _DOMUpdatesHandler_isCustomRenderEnabled, "f")) {
+                __classPrivateFieldGet(this, _DOMUpdatesHandler_instances, "m", _DOMUpdatesHandler_handleObjectStateRendering).call(this, state, element);
                 continue;
             }
-            __classPrivateFieldGet(this, _DOMHandler_instances, "m", _DOMHandler_handleNativeRendering).call(this, state, element);
+            __classPrivateFieldGet(this, _DOMUpdatesHandler_instances, "m", _DOMUpdatesHandler_handleSingleValueStateRendering).call(this, state, element);
         }
     }
 }
-_DOMHandler_stateElements = new WeakMap(), _DOMHandler_stateId = new WeakMap(), _DOMHandler_stateWrapper = new WeakMap(), _DOMHandler_isCustomRenderEnabled = new WeakMap(), _DOMHandler_onRender = new WeakMap(), _DOMHandler_instances = new WeakSet(), _DOMHandler_handleCustomRendering = function _DOMHandler_handleCustomRendering(state, element) {
-    const content = __classPrivateFieldGet(this, _DOMHandler_onRender, "f").call(this, state);
-    if (typeof content === 'string') {
-        if (element.innerHTML !== content) {
-            element.innerHTML = content;
-        }
-    }
-    else {
+_DOMUpdatesHandler_stateElements = new WeakMap(), _DOMUpdatesHandler_stateId = new WeakMap(), _DOMUpdatesHandler_stateWrapper = new WeakMap(), _DOMUpdatesHandler_isCustomRenderEnabled = new WeakMap(), _DOMUpdatesHandler_onRender = new WeakMap(), _DOMUpdatesHandler_instances = new WeakSet(), _DOMUpdatesHandler_handleObjectStateRendering = function _DOMUpdatesHandler_handleObjectStateRendering(state, element) {
+    if (typeof state !== 'object')
+        return;
+    const content = __classPrivateFieldGet(this, _DOMUpdatesHandler_onRender, "f").call(this, state);
+    if (typeof content !== 'string')
         throw new Error('onRender must return a string');
-    }
-}, _DOMHandler_handleNativeRendering = function _DOMHandler_handleNativeRendering(state, element) {
-    const types = ['string', 'number', 'bigint'];
-    if (isOneOfType(types, state)) {
+    if (element.innerHTML !== content)
+        element.innerHTML = content;
+}, _DOMUpdatesHandler_handleSingleValueStateRendering = function _DOMUpdatesHandler_handleSingleValueStateRendering(state, element) {
+    if (typeof state === 'string' || typeof state === 'number' || typeof state === 'bigint') {
         const newContent = `${state}`;
-        if (element.textContent !== newContent) {
-            element.textContent = newContent;
-        }
+        // if newContent is the same as the current content, do nothing
+        if (element.textContent === newContent)
+            return;
+        // otherwise, update the text content of the element
+        element.textContent = newContent;
+        return;
     }
-    else if (typeof state === 'boolean') {
+    if (typeof state === 'boolean') {
+        // boolean states are used to toggle the visibility of an element
         const display = state ? '' : 'none';
-        if (element.style.display !== display) {
-            element.style.display = display;
-        }
+        if (element.style.display === display)
+            return;
+        element.style.display = display;
     }
 };
-export class AppState {
-    get stateId() {
-        return __classPrivateFieldGet(this, _AppState_stateId, "f");
+export class State {
+    // since stateId and stateCurrent are readonly properties
+    // they are declared as private and a getter is provided
+    get id() {
+        return __classPrivateFieldGet(this, _State_stateId, "f");
     }
-    get stateCurrent() {
-        return __classPrivateFieldGet(this, _AppState_stateCurrent, "f");
+    get current() {
+        return __classPrivateFieldGet(this, _State_stateCurrent, "f");
     }
-    constructor({ stateId, stateWrapper = document.body, stateInitial, onStateChange = () => { }, onRender }) {
-        _AppState_instances.add(this);
-        _AppState_stateId.set(this, void 0);
-        _AppState_stateInitial.set(this, void 0);
-        _AppState_stateCurrent.set(this, void 0);
-        _AppState_isCustomRenderEnabled.set(this, void 0);
-        _AppState_domHandler.set(this, void 0);
-        if (!stateId || typeof stateId !== 'string') {
-            throw new Error('stateId must be a non-empty string');
-        }
-        if (!(stateWrapper instanceof HTMLElement)) {
+    constructor({ id, initial, wrapper = document.body, onChange = () => { }, onRender }) {
+        _State_instances.add(this);
+        _State_stateId.set(this, void 0);
+        _State_stateInitial.set(this, void 0);
+        _State_stateCurrent.set(this, void 0);
+        _State_isCustomRenderEnabled.set(this, void 0);
+        _State_domHandler.set(this, void 0);
+        if (!id || typeof id !== 'string')
+            throw new Error('id must be a non-empty string');
+        if (!(wrapper instanceof HTMLElement))
             throw new Error('stateWrapper must be an instance of HTMLElement');
-        }
-        __classPrivateFieldSet(this, _AppState_stateId, stateId, "f");
-        __classPrivateFieldSet(this, _AppState_stateInitial, stateInitial, "f");
-        __classPrivateFieldSet(this, _AppState_stateCurrent, stateInitial, "f");
-        this.onStateChange = onStateChange;
-        __classPrivateFieldSet(this, _AppState_isCustomRenderEnabled, onRender != null, "f");
+        __classPrivateFieldSet(this, _State_stateId, id, "f");
+        __classPrivateFieldSet(this, _State_stateInitial, initial, "f");
+        __classPrivateFieldSet(this, _State_stateCurrent, initial, "f");
+        this.onStateChange = onChange;
+        // if an onRender function is provided, it will be used to render the state
+        // therefore 'custom rendering' will be enabled
+        // otherwise, the default rendering will be used
+        __classPrivateFieldSet(this, _State_isCustomRenderEnabled, onRender != null, "f");
         if (onRender != null)
             this.onRender = onRender;
         else
             this.onRender = () => ``;
-        __classPrivateFieldSet(this, _AppState_domHandler, new DOMHandler(stateId, stateWrapper, __classPrivateFieldGet(this, _AppState_isCustomRenderEnabled, "f"), this.onRender), "f");
-        __classPrivateFieldGet(this, _AppState_domHandler, "f").updateElements(__classPrivateFieldGet(this, _AppState_stateCurrent, "f"));
+        // create an dom handler instance to manage the DOM updates
+        __classPrivateFieldSet(this, _State_domHandler, new DOMUpdatesHandler(id, wrapper, __classPrivateFieldGet(this, _State_isCustomRenderEnabled, "f"), this.onRender), "f");
+        // update dom elements with the initial state
+        __classPrivateFieldGet(this, _State_domHandler, "f").updateDOMElements(__classPrivateFieldGet(this, _State_stateCurrent, "f"));
     }
-    updateState(state) {
-        __classPrivateFieldGet(this, _AppState_instances, "m", _AppState_changeState).call(this, state);
-        __classPrivateFieldGet(this, _AppState_domHandler, "f").updateElements(__classPrivateFieldGet(this, _AppState_stateCurrent, "f"));
+    // this method is used to change the state value and update the DOM
+    update(state) {
+        __classPrivateFieldGet(this, _State_domHandler, "f").updateDOMElements(state);
+        __classPrivateFieldGet(this, _State_instances, "m", _State_changeState).call(this, state);
     }
-    setInitialState() {
-        __classPrivateFieldGet(this, _AppState_instances, "m", _AppState_changeState).call(this, __classPrivateFieldGet(this, _AppState_stateInitial, "f"));
-        __classPrivateFieldGet(this, _AppState_domHandler, "f").updateElements(__classPrivateFieldGet(this, _AppState_stateCurrent, "f"));
+    // this method resets the state to the initial state
+    reset() {
+        __classPrivateFieldGet(this, _State_instances, "m", _State_changeState).call(this, __classPrivateFieldGet(this, _State_stateInitial, "f"));
+        __classPrivateFieldGet(this, _State_domHandler, "f").updateDOMElements(__classPrivateFieldGet(this, _State_stateCurrent, "f"));
     }
 }
-_AppState_stateId = new WeakMap(), _AppState_stateInitial = new WeakMap(), _AppState_stateCurrent = new WeakMap(), _AppState_isCustomRenderEnabled = new WeakMap(), _AppState_domHandler = new WeakMap(), _AppState_instances = new WeakSet(), _AppState_changeState = function _AppState_changeState(state) {
-    const prevState = __classPrivateFieldGet(this, _AppState_stateCurrent, "f");
-    __classPrivateFieldSet(this, _AppState_stateCurrent, state, "f");
+_State_stateId = new WeakMap(), _State_stateInitial = new WeakMap(), _State_stateCurrent = new WeakMap(), _State_isCustomRenderEnabled = new WeakMap(), _State_domHandler = new WeakMap(), _State_instances = new WeakSet(), _State_changeState = function _State_changeState(state) {
+    const prevState = __classPrivateFieldGet(this, _State_stateCurrent, "f");
+    __classPrivateFieldSet(this, _State_stateCurrent, state, "f");
     this.onStateChange(state, prevState);
 };
